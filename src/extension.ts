7f328e3cef3e53fdd82a02382aca145d05f7f992
Adam Israel <adam.israel@canonical.com>
2020-02-16 00:20:51 -0500

    Fixes
    Fix some issues post-renaming the extension

--- src/extension.ts
+++ src/extension.ts
@@ -10,8 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "writers-wordcount" is now active!');
-
+	console.log('Congratulations, your extension "author-stats" is now active!');
 
     // create a new word counter
     let wordCounter = new WordCounter();
